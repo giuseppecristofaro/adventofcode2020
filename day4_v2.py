176e2982a54c6ccaa6e6aa8782485bd646ac594b
giuseppecristofaro <cristofaro.giu@gmail.com>
2020-12-04 19:28:57 +0100

    Update day4_v2.py

--- day4_v2.py
+++ day4_v2.py
@@ -1,10 +1,10 @@
-import re, json
+import re
 
 fields, eyes = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}, {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
 
 def read(filename):
     with open(filename, "r") as f:
-        data = [i.replace("\n", " ").strip() for i in f.read().split("\n\n")]
+        data = f.read().split("\n\n")
     return data
 
 def check_range(item, start, end):
@@ -40,7 +40,10 @@ def check_all(item):
 def check(data):
     count, count2 = 0, 0
     for item in data:
-        item_dict = json.loads('{"%s"}' % item.replace(':', '":"').replace(' ', '","'))
+        item_dict = {}
+        for i in item.split():
+            key, value = i.split(":")
+            item_dict[key] = value
         count += not fields - set(re.sub(r":[a-zA-Z0-9#]+|cid","", item).split())
         count2 += check_all(item_dict)
     return count, count2
